8ca4a354e1cd0c72b8b3253c97a8ac1566e3751e
HtetHtetAung <htethtetaung.htd24@gmail.com>
2017-11-23 14:41:41 +0630

    add text

--- hello.php
+++ hello.php
@@ -1,6 +1,17 @@
 <?php 
 
 	print "Hello World.";
+	print "hello Git Example";
+
 
 
  ?>
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title></title>
+ </head>
+ <body>
+ <input type="text" name="">
+ </body>
+ </html>
